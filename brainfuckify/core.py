aa163ee6581da98bd5fe3000019465e5e1b46d0d
Rossi Lorenzo <snowycoder@gmail.com>
2017-08-28 15:07:23 +0200

    Add comments

--- brainfuckify/core.py
+++ brainfuckify/core.py
@@ -9,7 +9,10 @@ def encode_chr(i):
 
 
 def encode(text):
-    return __encode(text).replace('<>', '')
+    computed = __encode(text)
+    if len(computed) > 0:
+        computed.pop(-1)
+    return ''.join(computed).replace('<>', '')
 
 
 def __encode(text):
@@ -19,28 +22,28 @@ def __encode(text):
         next_chr = ord(c)
         if next_chr > 127:
             continue
+        # If the next char is the same as the last one
         if next_chr is last:
+            # First check if there's no more space
+            if len(brainfuck) >= LIMIT:
+                return brainfuck
+            # Then add a print instruction between the last . and the < char
             brainfuck.insert(-1, '.')
         else:
-            brainfuck += encode_chr(next_chr - last) + '.<'
-        if len(brainfuck) >= LIMIT:
-            return ''.join(brainfuck)[:LIMIT]
-        last = next_chr
-    if len(brainfuck) > 0:
-        brainfuck.pop(-1)
-    return ''.join(brainfuck)
+            # Compute how much text the next char would take
+            next = encode_chr(next_chr - last) + '.<'
+            # And check if there's space to add it
+            if len(brainfuck) + len(next) > LIMIT:
+                # If there's no space left return the string up to the last char
+                return brainfuck
+            # Add the text to the buffer
+            brainfuck += next
+            # Update the value
+            last = next_chr
+    return brainfuck
 
 
-def largest_divisor(n):
-    i = int(n/2)
-    while i != 0:
-        if n % i == 0:
-            return i
-        i -= 1
-    return 1
-
-
-def isprime(n):
+def is_prime(n):
     """Returns True if n is prime."""
     if n == 2:
         return True
@@ -63,23 +66,27 @@ def isprime(n):
 
     return True
 
+
+# Returns all the possible multipliers that have the second value lesser than 10
 def __possible_mults(n):
     res = []
     i = int(n / 2)
     while i != 0:
         if n % i == 0:
-            b = int(n/i)
+            b = int(n / i)
             res.append((i, b))
             if b > 10:
                 return res
         i -= 1
     return res
 
+
+# deconstructs the given value into a*b+c
 def __decompose(i):
     if i <= 5:
-        return 0, i, 1
+        return 1, i, 0
 
-    if isprime(i):
+    if is_prime(i):
         off = 1
         i -= 1
     else:
@@ -91,42 +98,28 @@ def __decompose(i):
         if not chosen or abs(chosen[0] - chosen[1]) > abs(choice[0] - choice[1]):
             chosen = choice
 
-    return off, chosen[1], chosen[0]
+    return chosen[1], chosen[0], off
 
 
-def __smallify(numbs):
-    for x in range(0, len(numbs)):
-        if x > 5:
-            continue
-        for y in range(x + 1, len(numbs)):
-            if y > 5:
-                continue
-            r = numbs[x] * numbs[y]
-            if r <= 5:
-                numbs[x] = r
-                del numbs[y]
-                numbs.sort(reverse=True)
-                __smallify(numbs)
-                return
-
-
+# Fills the buffer map used to get the brainfuck source given the value difference
 def __build_map():
-    decompose_map[0] = '>'
+    decompose_map[0] = ''
     for i in range(1, 255):
         res = __decompose(i)
         decompose_map[i] = mul_to_brainfuck(res, False)
         decompose_map[-i] = mul_to_brainfuck(res, True)
 
 
+# Transforms the a*b+c form into a[>b<]c brainfuck code, negating it if possible
 def mul_to_brainfuck(tup, negative):
-    off, a, b = tup
+    a, b, off = tup
     ch = '-' if negative else '+'
     if a is 1 or b is 1:
-        return '>' + (ch * (a*b+off))
+        return '>' + (ch * (a * b + off))
 
     res = ('+' * a) + '[>' + (ch * b) + '<-]>' + (ch * off)
     return res
 
 
 __build_map()
-#print(encode('W JESÙ'))
+#print(encode('W la figa'))
