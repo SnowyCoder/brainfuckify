02f707f761afee4ce99c35e3100346ac4569f150
Rolando Sartorello <rolando.sartorello@gmail.com>
2017-08-28 16:56:27 +0200

    Lookup table for prime checking efficience

--- brainfuckify/core.py
+++ brainfuckify/core.py
@@ -45,6 +45,10 @@ def __encode(text):
 
 def is_prime(n):
     """Returns True if n is prime."""
+    # ASCII are espected to be encoded in one byte. a Lookup table is faster and cheaper
+    return n in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127]
+    
+''' 
     if n == 2:
         return True
     if n == 3:
@@ -65,7 +69,7 @@ def is_prime(n):
         w = 6 - w
 
     return True
-
+'''
 
 # Returns all the possible multipliers that have the second value lesser than 10
 def __possible_mults(n):
